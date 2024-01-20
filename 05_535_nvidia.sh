e0586d00a74b29e3e9c2fd037d0d3506761b32d9
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-01-20 10:37:43 -0300

    Add files via upload

--- 05_535_nvidia.sh
+++ 05_535_nvidia.sh
@@ -8,4 +8,5 @@ yay -Sy --needed nvidia-535xx-utils
 yay -S --needed nvidia-535xx-dkms
 yay -S --needed lib32-nvidia-535xx-utils
 yay -S --needed nvidia-535xx-settings
+yay -Sy opencl-nvidia-535xx
 yay -S --needed vulkan-icd-loader lib32-vulkan-icd-loader
