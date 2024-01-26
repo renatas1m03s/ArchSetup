9575f888dcfbf52bf1cbee257196b8b5e7665c2e
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-01-26 05:24:09 -0300

    Add files via upload

--- 05_nvidia_notebook.sh
+++ 05_nvidia_notebook.sh
@@ -2,7 +2,12 @@
 
 sudo pacman -Sy --noconfirm archlinux-keyring
 
-sudo pacman -Sy --noconfirm nvidia nvidia-zen nvidia-libgl mesa opencl-nvidia
+# sudo pacman -Sy --noconfirm nvidia nvidia-zen nvidia-libgl mesa opencl-nvidia
+
+yay -Sy --needed nvidia-utils-beta nvidia-beta-dkms mesa
+yay -S --needed lib32-nvidia-utils-beta nvidia-settings-beta
+yay -S --needed opencl-nvidia-beta
+yay -S --needed vulkan-icd-loader lib32-vulkan-icd-loader
 
 yay -Sy --noconfirm supergfxctl
 
