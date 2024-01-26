95b0c47b452bd596006812bbdcf55bdbe65601e4
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-01-26 05:20:52 -0300

    Add files via upload

--- 04_pacman.sh
+++ 04_pacman.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+# sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+# sudo pacman-key --lsign-key 3056513887B78AEB
+# sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-echo -e '[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
+# echo -e '[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
 
 sudo pacman -Sy --noconfirm --needed base-devel git wget yajl
 cd /tmp && git clone https://aur.archlinux.org/package-query.git && cd package-query/ && makepkg -si && cd /tmp
