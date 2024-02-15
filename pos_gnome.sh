776f37d771d10484eff91e50752926e6e80f72e6
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-02-15 12:06:41 -0300

    Add files via upload

--- pos_gnome.sh
+++ pos_gnome.sh
@@ -20,7 +20,7 @@ cp -v /mnt/Backup/Home/.local/share/applications/launcher.desktop /home/renata/.
 cp -v /mnt/Backup/Home/.local/share/applications/DaVinciResolve.desktop /home/renata/.local/share/applications/
 cp -v /mnt/Backup/Home/.local/share/applications/geany.desktop /home/renata/.local/share/applications/
 
-# cp -rv /mnt/Backup/Home/.local/share/icons/* ~/.local/share/icons
+sudo rsync -axv --progress /mnt/Backup/Home/.local/share/icons/Tel* ~/.local/share/icons/
 
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 sudo chmod a+rx /usr/local/bin/yt-dlp
