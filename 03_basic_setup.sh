d61442ef634e24c91cc104cfe4c94c2c791650fe
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-07-29 11:46:16 -0300

    Add files via upload

--- 03_basic_setup.sh
+++ 03_basic_setup.sh
@@ -4,12 +4,15 @@ sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 8/g' /etc/pacman.conf
 sudo sed -i 's/#Color/Color\nILoveCandy/g' /etc/pacman.conf
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
+reflector --sort rate --latest 10 --save /etc/pacman.d/mirrorlist
+
 echo -e '\nConfigurando timezone e locales\n'
 
 ln -sf /usr/share/zoneinfo/America/Fortaleza /etc/localtime
+sed -i s/#pt_BR.UTF-8/pt_BR.UTF-8/g /etc/locale.gen
 sed -i s/#en_US.UTF-8/en_US.UTF-8/g /etc/locale.gen && locale-gen && echo LANG=en_US.UTF-8 >> /etc/locale.conf
-# echo KEYMAP==us-acentos >> /etc/vconsole.conf
-echo KEYMAP==br-abnt2 >> /etc/vconsole.conf
+echo KEYMAP=us-acentos >> /etc/vconsole.conf
+# echo KEYMAP=br-abnt2 >> /etc/vconsole.conf
 
 echo -e '\n Instalando editores de texto básicos e o fish shell\n'
 
@@ -37,7 +40,7 @@ sudo usermod renata -aG libvirt
 echo -e '\nInstalando os pacotes básicos e habilitando o NetworkManager e sshd\n'
 
 pacman -Sy --noconfirm dosfstools os-prober mtools networkmanager dialog sudo
-pacman -Sy --noconfirm rsync grub-efi-x86_64 efibootmgr openssh exfat-utils 
+pacman -Sy --noconfirm rsync grub-efi-x86_64 efibootmgr openssh exfat-utils plymouth
 
 systemctl enable NetworkManager && systemctl enable sshd
 
@@ -45,12 +48,12 @@ echo -e '\nConfigurando o GRUB com thema'
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
 
-# sed -i 's/quiet/quiet nvme_load=YES/g' /etc/default/grub
-sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
-
-# quiet loglevel=3 splash nvme_load=YES split_lock_detect=off
+sed -i 's/loglevel=3 quiet/quiet loglevel=3 splash split_lock_detect=off nvidia_drm.modeset=1/g' /etc/default/grub
+# sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
+sed -i 's/block filesystems fsck/block filesystems fsck plymouth/g' /etc/mkinitcpio.conf
 
 echo -e '\nDefinindo os parâmetros do SSHD\n'
 
