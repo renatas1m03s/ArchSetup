a1aeb083ebb4e43142ef6295860a9ece8c98175c
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-07-29 14:30:41 -0300

    Add files via upload

--- 0A_gaming.sh
+++ 0A_gaming.sh
@@ -4,8 +4,10 @@
 # cd /tmp && git clone https://github.com/medusalix/xone && cd xone
 # sudo ./install.sh --release
 
-yay -Sy kpat steam-native-runtime protonup-qt retroarch-git emulationstation-de pcsx2-git gamescope
+# yay -Sy --noconfirm kpat steam-native-runtime protonup-qt retroarch-git emulationstation-de pcsx2-git gamescope
 
-sudo setcap 'CAP_SYS_NICE=eip' /usr/bin/gamescope
+yay -Sy --noconfirm kpat steam-native-runtime protonup-qt retroarch-git emulationstation-de pcsx2-git gamescope-plus
+
+# sudo setcap 'CAP_SYS_NICE=eip' /usr/bin/gamescope
 
 
