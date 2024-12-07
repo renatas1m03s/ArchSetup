136ee37cf2a3f70bc31a8cfe0a706e7ad07fb07d
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2024-12-07 11:35:46 -0300

    Delete 05_nvidia_open.sh

--- 05_nvidia_open.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Sy --noconfirm archlinux-keyring
-
-sudo pacman -S --needed --noconfirm nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader nvidia-libgl
