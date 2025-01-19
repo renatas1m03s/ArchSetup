6a138594a22f6ec516c981e0a887e2c347aa55ed
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2025-01-19 14:13:27 -0300

    Add files via upload

--- plasma.sh
+++ plasma.sh
@@ -2,15 +2,15 @@
 
 yay -Sy --needed  --noconfirm plasma-meta xorg-xlsclients bluez-utils blueman cronie  
 
-yay -S --needed  --noconfirm packagekit-qt6 gnome-disk-utility konsole inetutils dnsutils conky
+yay -S --needed  --noconfirm packagekit-qt6 gnome-disk-utility konsole inetutils dnsutils conky alacritty
 
 yay -S --needed  --noconfirm okular dolphin ark spectacle gwenview geany kcalc openconnect networkmanager-openconnect
 
 yay -S --needed  --noconfirm kio kio-extras ffmpegthumbs kdegraphics-thumbnailers
 
-yay -S --needed  --noconfirm kimageformats qt6-imageformats kdesdk-thumbnailers neofetch openvpn
+yay -S --needed  --noconfirm kimageformats qt6-imageformats kdesdk-thumbnailers tuned tuned-ppd
 
-yay -S --needed  --noconfirm vlc neofetch openvpn google-chrome usb_modeswitch update-grub nufraw geany p7zip btop pamac-tray-icon-plasma pamac-aur
+yay -S --needed  --noconfirm vlc fastfetch openvpn google-chrome usb_modeswitch update-grub nufraw geany p7zip btop pamac-tray-icon-plasma pamac-aur
 
 yay -S --needed  --noconfirm adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji
 
@@ -25,6 +25,9 @@ sudo systemctl enable sddm
 sudo systemctl enable bluetooth
 sudo systemctl enable cronie
 sudo systemctl enable ModemManager
+sudo systemctl enable tuned  
+sudo systemctl enable tuned-ppd
+
 
 cp -v ./functions/* /home/renata/.config/fish/functions/
 
